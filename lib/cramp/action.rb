7e0b2fa9f23fda37a0e1373c168a967623b6899e
Rachmat Maulana <maulana.rachmat@gmail.com>
2012-07-17 14:49:26 +0700

    change header

--- lib/cramp/action.rb
+++ lib/cramp/action.rb
@@ -36,7 +36,23 @@ module Cramp
     self.default_sse_headers = {'Content-Type' => 'text/event-stream', 'Cache-Control' => 'no-cache', 'Connection' => 'keep-alive'}
 
     class_attribute :default_chunked_headers
-    self.default_chunked_headers = {'Transfer-Encoding' => 'chunked', 'Connection' => 'keep-alive'}
+    self.default_chunked_headers = {
+      'Cache-Control' => 'no-cache, no-store, must-revalidate, pre-check=0, post-check=0',
+      'Content-Encoding' => 'deflate',
+      'Content-Type' => 'application/json;charset=utf-8',
+      'Date' => 'Tue, 17 Jul 2012 06:26:49 GMT',
+      'Expires' => 'Tue, 31 Mar 1981 05:00:00 GMT',
+      'Last-Modified' => 'Tue, 17 Jul 2012 06:26:49 GMT',
+      'Pragma' => 'no-cache',
+      'Server' => 'tfe',
+      'X-Firefox-Spdy' => '1',
+      'x-frame-options' => 'SAMEORIGIN',
+      'x-ratelimit-class' => 'api',
+      'x-ratelimit-limit' => '150',
+      'x-ratelimit-remaining' => '148',
+      'x-ratelimit-reset' => '1342509997',
+      'x-transaction' => '9ae86ab649b8cf4c'
+    }
 
     def build_headers
       case transport
@@ -44,32 +60,11 @@ module Cramp
         status, headers = respond_to?(:respond_with, true) ? respond_with : [200, {'Content-Type' => 'text/html'}]
         [status, headers.merge(self.default_sse_headers)]
       when :chunked
-#        status, headers = respond_to?(:respond_with, true) ? respond_with : [200, {}]
-#
-#        headers = headers.merge(self.default_chunked_headers)
-#        headers['Content-Type'] ||= 'text/html'
-#        headers['Cache-Control'] ||= 'no-cache'
-
-
         status, headers = respond_to?(:respond_with, true) ? respond_with : [200, {}]
-
-        headers.merge!({
-          'Cache-Control' => 'no-cache, no-store, must-revalidate, pre-check=0, post-check=0',
-          'Content-Encoding' => 'deflate',
-          'Content-Type' => 'application/json;charset=utf-8',
-          'Date' => 'Tue, 17 Jul 2012 06:26:49 GMT',
-          'Expires' => 'Tue, 31 Mar 1981 05:00:00 GMT',
-          'Last-Modified' => 'Tue, 17 Jul 2012 06:26:49 GMT',
-          'Pragma' => 'no-cache',
-          'Server' => 'tfe',
-          'X-Firefox-Spdy' => '1',
-          'x-frame-options' => 'SAMEORIGIN',
-          'x-ratelimit-class' => 'api',
-          'x-ratelimit-limit' => '150',
-          'x-ratelimit-remaining' => '148',
-          'x-ratelimit-reset' => '1342509997',
-          'x-transaction' => '9ae86ab649b8cf4c'
-        })
+        
+        headers = headers.merge(self.default_chunked_headers)
+        headers['Content-Type'] ||= 'text/html'
+        headers['Cache-Control'] ||= 'no-cache'
 
         [status, headers]
       else
